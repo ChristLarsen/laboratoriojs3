f8d6937cf15618088b4f4e882005fca48ec88a51
ChristLarsen <c.larsen@mayoral.com>
2024-10-29 15:06:48 +0100

    Commit inicial

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+console.log("Cena. Somos 6 amigos");
+console.log("Total de la cena-> 120 €");
+console.log("Importe de bebidas-> 18 €");
+
+// prueba
+console.log("Importe a pagar por la cena (las bebidas las pago yo)->" + (120-18));
+console.log("Pues cada uno tiene que pagar->" + ((120-18)/6));
+console.log("menos yo, que pago además los 18€ de las bebidas->" + (((120-18)/6)+18));
